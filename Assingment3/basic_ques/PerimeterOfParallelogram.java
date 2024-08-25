4ffa06cce4d71c656996d1d32f9bf836fb1c361d
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-25 15:46:40 +0530

    .

---
+++ Assingment3/basic_ques/PerimeterOfParallelogram.java
@@ -0,0 +1,11 @@
+package Assingment3.basic_ques;
+
+public class PerimeterOfParallelogram {
+    public static void main(String[] args)
+    {
+        int a = 3;
+        int b = 4;
+        int p = 2*(a+b);
+        System.out.println("perimeter of parallelogram "+p);
+    }
+}
