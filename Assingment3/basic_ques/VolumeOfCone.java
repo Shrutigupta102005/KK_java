dca629120daffca707be3dcc30051a11ed2e4b0b
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-25 16:08:51 +0530

    .

---
+++ Assingment3/basic_ques/VolumeOfCone.java
@@ -0,0 +1,11 @@
+package Assingment3.basic_ques;
+
+public class VolumeOfCone {
+    public static void main(String[] args)
+    {
+        int r=3;
+        int h=4;
+        double  v =(1.0/3)*r*r*h*3.14;  // its possible you are getting the output as 0 when you are using 1/3
+        System.out.println(v);
+    }
+}
