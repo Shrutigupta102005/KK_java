6a1af4b2ef4f2f4c81b4842045b64c42ed95ad39
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-25 15:26:39 +0530

    .

--- Assingment3/basic_ques/EquilateralTriangle7.java
+++ Assingment3/basic_ques/EquilateralTriangle7.java
@@ -1,5 +1,10 @@
 package Assingment3.basic_ques;
 
 public class EquilateralTriangle7 {
-    
+    public static void main(String[] args)
+    {
+        int a= 5;
+        double area = (Math.pow(3,1/2)/4 )* a * a ;
+        System.out.println("Area of triangle is "+area);
+    }
 }
