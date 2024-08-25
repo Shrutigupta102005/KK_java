bfc30ddaeb8f04fb9c2996717cb2ba810a87be7b
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-25 15:40:10 +0530

    .

---
+++ Assingment3/basic_ques/EquilateralTriangle9.java
@@ -0,0 +1,10 @@
+package Assingment3.basic_ques;
+
+public class EquilateralTriangle9 {
+    public static void main(String[] args)
+    {
+        int a = 22;
+        double Perimeter = 3 * a ;
+        System.out.println("Perimeter of Equilateraltriangle is "+Perimeter);
+    }
+}
---
+++ Assingment3/basic_ques/circle8.java
@@ -0,0 +1,10 @@
+package Assingment3.basic_ques;
+
+public class circle8 {
+        public static void main(String[] args)
+        {
+            int radius = 22;
+            double Perimeter = 3.14 * radius *2 ;
+            System.out.println("Perimeter of circle is "+Perimeter);
+        }
+}
