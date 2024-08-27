81d1c20322e19440de818ff7ce51904a6edca207
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-27 22:54:52 +0530

    .

---
+++ Assingment9/Pattern10.java
@@ -0,0 +1,15 @@
+package Assingment9;
+
+public class Pattern10 {
+    public static void main(String[] args){
+
+
+    }
+}
+/*       *
+        * *
+       * * *
+      * * * *
+     * * * * * 
+     * */
+       
