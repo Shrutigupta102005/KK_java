04628ee3abc675a584028241171952fefa96ee0d
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-31 21:16:48 +0530

    .

---
+++ Assingment4/Evenodd.java
@@ -0,0 +1,15 @@
+package Assingment4;
+
+public class Evenodd {
+    public static void main(String[] args){
+        evenodd(4);
+        evenodd(5);
+    }
+    static void evenodd(int a){
+        if (a%2==0){
+            System.out.println("even");
+        }
+        else
+        System.out.println("odd");
+    }
+}
---
+++ Assingment4/vote.java
@@ -0,0 +1,18 @@
+package Assingment4;
+
+public class vote {
+    public static void main(String[] args){
+        vote(3);
+
+    }
+    static void vote(int age ){
+        if (age>=18){
+            System.out.println("eligable voter");
+        }
+        else
+        {
+            System.out.println("not an eligble voter");
+        }
+
+    }
+}
